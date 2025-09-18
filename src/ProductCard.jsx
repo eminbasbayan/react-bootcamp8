68353f6c9d492811834b5b92c473522210e67458
Emin Basbayan <emin.basbayan@icloud.com>
2025-09-18 23:31:27 +0300

    css dosyası ile stillendirme

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,19 +1,18 @@
-const productCardCss = {
-  backgroundColor: 'black',
-  color: 'red',
-  textAlign: "end"
-};
+import './ProductCard.css';
 
 function ProductCard() {
   return (
-    <div className="product-card" style={productCardCss}>
+    <div className="product-card">
       <img
         className="product-image"
         src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
         alt="Çanta Görseli"
       />
-      <strong className="product-title">Çanta</strong>
-      <span className="product-price">1000₺</span>
+
+      <div className="product-info">
+        <strong className="product-title">Çanta</strong>
+        <span className="product-price">1000₺</span>
+      </div>
     </div>
   );
 }
