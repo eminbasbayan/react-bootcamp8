8d00f050c68904fc1fb1fe96cfa26072f2b33fc1
Emin Basbayan <emin.basbayan@icloud.com>
2025-09-25 23:05:49 +0300

    çoklu state ve çoklu function ile form yönetimi

--- src/components/Products/AddProductForm.jsx
+++ src/components/Products/AddProductForm.jsx
@@ -1,24 +1,64 @@
+import { useState } from 'react';
 import Button from '../UI/Button';
 import './AddProductForm.css';
 
 function AddProductForm() {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const [description, setDescription] = useState('');
+
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handlePriceChange(event) {
+    setPrice(event.target.value);
+  }
+
+  function handleImageChange(event) {
+    setImage(event.target.value);
+  }
+
+  function handleDescriptionChange(event) {
+    setDescription(event.target.value);
+  }
+
   return (
     <form className="add-product-form">
+      {title}
       <label>
-        Title:
-        <input type="text" placeholder="Bir ürün ismi giriniz!" />
+        Title: {title}
+        <input
+          type="text"
+          onChange={handleTitleChange}
+          placeholder="Bir ürün ismi giriniz!"
+        />
       </label>
       <label>
-        Price:
-        <input type="number" placeholder="Bir ürün fiyatı giriniz!" />
+        Price: {price}
+        <input
+          type="number"
+          onChange={handlePriceChange}
+          placeholder="Bir ürün fiyatı giriniz!"
+        />
       </label>
       <label>
-        Image URL:
-        <input type="text" placeholder="Bir ürün görseli giriniz!" />
+        Image URL: {image}
+        <input
+          type="text"
+          onChange={handleImageChange}
+          placeholder="Bir ürün görseli giriniz!"
+        />
       </label>
       <label>
-        Description:
-        <input type="text" placeholder="Bir ürün açıklaması giriniz!" />
+        Description: {description}
+        <input
+          type="text"
+          onChange={handleDescriptionChange}
+          placeholder="Bir ürün açıklaması giriniz!"
+        />
       </label>
 
       <Button type="primary">Ürünü Ekle</Button>
