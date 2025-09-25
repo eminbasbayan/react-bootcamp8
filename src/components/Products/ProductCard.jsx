7807ac98b32737bbad18b036e96522a4769e02f8
Emin Basbayan <emin.basbayan@icloud.com>
2025-09-25 21:39:48 +0300

    local ve parent state farkı

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,15 +1,24 @@
 import './ProductCard.css';
 
 function ProductCard(props) {
+
+  function handleTitleChange(){
+    props.setTitleState("Title Değişti!")
+  }
+
   return (
     <div className="product-card">
       <img className="product-image" src={props.image} alt="Çanta Görseli" />
 
       <div className="product-info">
-        <strong className="product-title">{props.title}</strong>
+        <strong className="product-title">{props.titleState}</strong>
         <span className="product-price">{props.price}₺</span>
         <p className='product-description'>{props.description}</p>
+
+        <button onClick={handleTitleChange}>Title'ı Değiştir!</button>
       </div>
+
+      
     </div>
   );
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,11 @@
 import ProductCard from './ProductCard';
 import productsData from '../../data/productsData';
 import './Products.css';
+import { useState } from 'react';
 
 function Products() {
+    const [titleState, setTitleState] = useState("Title");
+
   return (
     <div className="products">
       <h2>Products Component</h2>
@@ -16,6 +19,8 @@ function Products() {
               title={product.title}
               price={product.price}
               description={product.description}
+              titleState={titleState}
+              setTitleState={setTitleState}
             />
           );
         })}
