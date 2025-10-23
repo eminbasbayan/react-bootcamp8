876e3691e7e9c5c80b49623bbc6ed15771757e1b
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-23 23:29:41 +0300

    sepet toplam hesaplama işlemleri

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,6 +5,11 @@ import ProductCard from '../components/Products/ProductCard';
 
 const CartPage = () => {
   const { cartItems, removeFromCart } = useContext(CartContext);
+
+  const totalValue = cartItems.reduce((toplam, element) => {
+    return element.price * element.quantity + toplam;
+  }, 0);
+
   return (
     <div className="cart-page">
       <Header />
@@ -19,6 +24,10 @@ const CartPage = () => {
           />
         ))}
       </div>
+
+      <p className="my-10 text-2xl font-bold">
+        Sepet Toplam: {totalValue.toFixed(2)}{' '}
+      </p>
     </div>
   );
 };
